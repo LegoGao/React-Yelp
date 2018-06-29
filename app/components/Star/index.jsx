d615bd41b01bac0f21adceda95f1e55415adcb0a
Lego Gao <jasonusdc@gmail.com>
2018-06-28 23:26:21 -0700

    add .babelrc

--- app/components/Star/index.jsx
+++ app/components/Star/index.jsx
@@ -9,7 +9,7 @@ class Star extends React.Component {
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
     render() {
-        // Get Star Number and  number % 5（up to 5 stars）
+        // 获取 star 数量，并取余5（最多5个star）
         let star = this.props.star || 0
         if (star > 5) {
             star = star % 5
